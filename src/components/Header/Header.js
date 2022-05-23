15bae65a6a44b6b6ec9458d0800704b9c4720860
pyropiyush <sahupiyush91@gmail.com>
2022-05-23 12:23:52 +0530

    com2

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -12,7 +12,7 @@ function Header() {
           A <span>Resume</span> that stands out!
         </p>
         <p className={styles.heading}>
-          Make your own resume. <span>It's free</span>
+          A Step to Sucess. <span>It's free</span>
         </p>
       </div>
       <div className={styles.right}>
